f0747cfb5ddee50d4cfc2ee5212f76dcecc19c0b
hemhemakk <hemavathikrishnan2627@gmail.com>
2025-03-29 12:52:16 +0530

    success

---
+++ L1-Employee Performance Evaluation System.js
@@ -0,0 +1,24 @@
+function performanceEvaluation( empDetails){
+  
+    const task =  empDetails.filter(ele => ele.tasksCompleted > 5).sort((a, b) =>b.rating - a.rating).map(ele =>{
+        let performance;
+        if (ele.rating > 4.5) 
+         performance = "Excellent";
+        else if (ele.rating >= 3 && ele.rating <= 4.5) 
+         performance = "Good";
+        else 
+         performance = "Needs Improvement";
+    
+      return { name: ele.name, performance };
+    });
+    console.log(task);
+  }
+  
+  let empDetails = [
+      { name: "Alice", tasksCompleted: 8, rating: 4.7 },
+      { name: "Bob", tasksCompleted: 4, rating: 4.0 },
+      { name: "Charlie", tasksCompleted: 6, rating: 3.5 },
+      { name: "David", tasksCompleted: 10, rating: 4.9 },
+      { name: "Eve", tasksCompleted: 7, rating: 2.8 }
+    ];
+    performanceEvaluation(empDetails)
